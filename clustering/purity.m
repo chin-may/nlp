c6622b02d28493bc2f7d7732efc83bbc8344a2c1
chinmay <cdbapat92@gmail.com>
2013-11-22 17:50:39 +0530

    Fixed index bug

--- clustering/purity.m
+++ clustering/purity.m
@@ -1,15 +1,18 @@
 function [purity, wt_pur,clas]  = purity(centers, classes)
+% centers is a column containing the cluster to which each point is assigned
+% classes is a column containing the class to which each point belongs
 % For every cluster, purity contains the fraction of the max class in that 
 % cluster. wt_pur is the weighted average of purities over data points
 % classes contains the majority class of every cluster
 wt_pur = 0;
-for i=unique(centers)
+for i=1:size(unique(centers),1)
     locs = find(centers==i);
     labels = classes(locs);
     clas(i) = mode(labels);
-    purity(i) = sum(labels==mode(labels))/size(labels,2);
-    wt_pur =wt_pur+ purity(i) * size(labels,2);
+    purity(i) = sum(labels==mode(labels))/size(labels,1);
+    wt_pur =wt_pur+ purity(i) * size(labels,1);
 end
 
-wt_pur = wt_pur / size(centers,2);
+wt_pur = wt_pur / size(centers,1);
 purity=purity';
+clas = clas';
